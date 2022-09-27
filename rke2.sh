ef494d631b0bb26a07b80f587d94347a80ba8770
sumpf <sumpf@hanmail.net>
2022-09-27 13:28:26 +0900

    Create rke2.sh

---
+++ rke2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo -i
+swapoff -a
+apt-get install curl -y
+#curl -sfL https://get.rke2.io | INSTALL_RKE2_CHANNEL=v{rke2_version}+rke2r1 INSTALL_RKE2_TYPE="server" sh -
+
+systemctl enable rke2-server.service
+systemctl start rke2-server.service
+
+echo "rke2-server started"
+
+sleep 5m
+
+systemctl status rke2-server.service
+
+mkdir $HOME/.kube
+cp /etc/rancher/rke2/rke2.yaml $HOME/.kube/config
+cat /var/lib/rancher/rke2/server/node-token
+kubectl get nodes
